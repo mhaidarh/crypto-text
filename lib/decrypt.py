27052c84152d2f236c8c64e6169e3006b5557fa1
M Haidar Hanif <mhaidarhanif@gmail.com>
2015-12-15 00:20:39 +0700

    Modularize the project with lib import

--- lib/decrypt.py
+++ lib/decrypt.py
@@ -48,26 +48,3 @@ def jefferson_decrypt(string):
         result += chr(c)
     # Return transformation.
     return result
-
-
-def main():
-    filename  = raw_input("[?] Enter encrypted text file name: ")
-    output    = raw_input("[?] Enter decrypted output text file name: ")
-    key       = input("[?] Enter the key number (0-9): ")
-
-    file      = open(str(filename), 'r')
-    text      = file.read()
-    file.close()
-    decrypted = jefferson_decrypt(text)
-    decrypted = scytale_decrypt(decrypted, key)
-
-    print "[i] Encrypted: \n %s \n" % (text)
-    print "[i] Decrypted: \n %s \n" % (decrypted)
-
-    file = open(str(output), 'w')
-    file.write(decrypted)
-    file.close()
-
-
-if __name__ == '__main__':
-    main()
--- lib/encrypt.py
+++ lib/encrypt.py
@@ -51,26 +51,3 @@ def jefferson_encrypt(string):
         result += chr(c)
     # Return transformation.
     return result
-
-
-def main():
-    filename  = raw_input("[?] Enter text file name: ")
-    output    = raw_input("[?] Enter output text file name: ")
-    key       = input("[?] Enter the key number (0-9): ")
-
-    file      = open(str(filename), 'r')
-    text      = file.read()
-    file.close()
-    encrypted = scytale_encrypt(text, key)
-    encrypted = jefferson_encrypt(encrypted)
-
-    print "[i] Text: \n %s \n" % (text)
-    print "[i] Encrypted: \n %s \n" % (encrypted)
-
-    file = open(str(output), 'w')
-    file.write(encrypted)
-    file.close()
-
-
-if __name__ == '__main__':
-    main()
